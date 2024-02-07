d40b01ceffe1e1db5820105cb893977dc491e669
Rediner <rediner@hotmail.com>
2024-02-07 00:16:55 -0300

    Base de componente recompensas

--- src/app/recompensas/recompensas.component.ts
+++ src/app/recompensas/recompensas.component.ts
@@ -5,6 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './recompensas.component.html',
   styleUrl: './recompensas.component.scss'
 })
-export class RecompensasComponent {
 
+export class RecompensasComponent {
+  recompensas: any[];
+  atividadeDefault = '../assets/img/default.png';
+  
+  constructor() {
+    const storedAtividades = localStorage.getItem('recompensas');
+    this.recompensas = storedAtividades ? JSON.parse(storedAtividades) : [{ src: this.atividadeDefault, price: 0 }];
+  }
 }
